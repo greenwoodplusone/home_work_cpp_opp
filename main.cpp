3adc5a92dfd8d1ac146c064cf7bf8328610525f7
greenwoodplusone <greenwoodplusone@gmail.com>
2021-06-06 22:39:31 +0500

    home_work_1_1

--- main.cpp
+++ main.cpp
@@ -1,111 +1,36 @@
 /*
-Тема: Конструкторы, деструкторы
+Тема: Введение в объектно-ориентированное программирование.
 
 Задание.
-Создайте приложение "Телефонная книга". Необходимо
-хранить данные об абоненте (ФИО, домашний телефон,
-рабочий телефон, мобильный телефон, дополнительная
-информация о контакте) внутри соответствующего класса. Наполните класс переменными-членами, функциями-членами, конструкторами, inline-функциями-членами,
-используйте инициализаторы, реализуйте деструктор.
-Обязательно необходимо выделять динамически память
-под ФИО. Предоставьте пользователю возможность добавлять новых абонентов, удалять абонентов, искать абонентов по ФИО, показывать всех абонентов, сохранять
-информацию в файл и загружать из файла.
+Реализуйте класс Дробь. Необходимо хранить числитель
+и знаменатель в качестве переменных-членов. Реализуйте
+функции-члены для ввода данных в переменные-члены,
+для выполнения арифметических операций (сложение,
+вычитание, умножение, деление, и т.д.).
  */
 
 #include <iostream>
-#include "Subscriber.h"
-#include "PhoneBook.h"
-
-using namespace std;
+#include "Fraction.h"
 
 int main()
 {
-    setlocale(LC_ALL, "");
+    Fraction fraction = Fraction();
+    fraction.addition(1, 2, 3, 4);
+    cout << endl;
+    fraction.addition(2, 4, 1, 3);
+    cout << endl;
 
-    // Добавление новой телефонной книги
-    PhoneBook ph1 = PhoneBook();
+    fraction.subtraction(3, 2, 5, 6);
+    cout << endl;
+    fraction.subtraction(1, 3, 4, 7);
+    cout << endl;
 
-    do {
-        cout << "Меню:" << endl;
-        cout << "1. Добавить пользователя" << endl;
-        cout << "2. Удалить пользователя" << endl;
-        cout << "3. Вывести всех пользователей" << endl;
-        cout << "4. Поиск пользователя по ФИО" << endl;
-        cout << "5. Завершить программу" << endl;
+    fraction.multiplication(2, 7, 3, 2);
+    cout << endl;
+    fraction.multiplication(4, 5, 1, 6);
+    cout << endl;
 
-        cout << "Выберите пункт меню - ";
-        int input;
-        cin >> input;
-
-        cout << endl;
-
-        switch (input)
-        {
-            case 1:
-                ph1.addName();
-                break;
-            case 2:
-                cout << "Введите id пользователя - ";
-                int id;
-                cin >> id;
-                cout << endl;
-                cout << "Пользователь " << (*(ph1.getName(id))).getName() << " " << (*(ph1.getName(id))).getFirstName() << " удален" << endl;
-                ph1.deleteName(id);
-                cout << endl;
-                break;
-            case 3:
-                for (int i = 0; i < 255; i++) {
-                    if (ph1.getName(i) != nullptr) {
-                        cout << "Пользователь № " << i << endl;
-                        (*(ph1.getName(i))).printSubscriber();
-                        cout << endl;
-                    }
-                }
-                break;
-            case 4:
-                {
-                    cout << "Введите имя пользователя - ";
-                    char* nameMain = new char[255];
-                    cin >> nameMain; 
-
-                    cout << endl;
-
-                    cout << "Введите фамилию пользователя - ";
-                    char* firstNameMain = new char[255];
-                    cin >> firstNameMain;
-                    cout << endl;
-
-                    Subscriber* subscriber = ph1.searchName(nameMain, firstNameMain);
-
-                    if (subscriber != nullptr) {
-                        cout << "Найден пользователь: " << endl;
-                        (*subscriber).printSubscriber();
-                        cout << endl;
-                    } else {
-                        cout << "Пользователь не найден!" << endl;
-                        cout << endl;
-                    } 
-
-                    nameMain = nullptr;
-                    delete[] nameMain;
-                    firstNameMain = nullptr;
-                    delete[] firstNameMain;
-                }
-                
-                break;
-            case 5:
-                return 0;
-            default:
-                cout << "Выбран несуществующий пункт меню!" << endl;
-                break;
-        }
-    } while (true);
-
-    /*
-    Subscriber sub1 = Subscriber("Ivan0", "Ivanov0", "Ivanovich0", 2741712, 2745962, 927947, "Информация");
-    cout << sub1.getName() << endl;
-    sub1.printSubscriber();
-    */
-
-    return 0;
+    fraction.division(1, 3, 4, 5);
+    cout << endl;
+    fraction.division(7, 8, 2, 3);
 }
