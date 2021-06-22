9f7241d0bd6608c7763ac28e7f0b2e3eef363179
greenwoodplusone <greenwoodplusone@gmail.com>
2021-06-22 10:21:02 +0500

    home_work_3_3 (go)

--- MyString.cpp
+++ MyString.cpp
@@ -1,2 +1,51 @@
 #include "MyString.h"
 
+MyString::MyString (int strLength) {
+    str = new char[strLength] {NULL};
+    this->strLength = strLength;
+
+    std::cout << "Конструктор с установкой длины - " << this << std::endl;
+    std::cout << std::endl;
+
+    MyString::strCount++;
+}
+
+MyString::MyString () : MyString (80){
+    std::cout << "Конструктор по умолчанию (делегировал) - " << this << std::endl;
+    std::cout << std::endl;
+}
+
+MyString::MyString (char* strInput) : str {strInput} {
+    strLength = strlen(str);
+    std::cout << "Конструктор принимающий строку - " << this << std::endl;
+    std::cout << std::endl;
+
+    MyString::strCount++;
+}
+
+MyString::MyString (const MyString& strP) : str(strP.str), strLength(strP.strLength){
+    std::cout << "Конструктор копирования объекта - " << &strP << std::endl;
+
+    MyString::strCount++;
+}
+
+MyString::~MyString () {
+    std::cout << "Уничтожен объект " << this << std::endl;
+    std::cout << std::endl;
+    str = nullptr;
+    delete str;
+}
+
+MyString& MyString::addStr () {
+    std::cout << "Введите строку: " << std::endl;
+    std::cin.getline(str, strLength); // проверка на переполнение строки
+
+    return *this;
+}
+
+MyString& MyString::printStr () {
+    std::cout << str << std::endl;
+    std::cout << "Выведена строка длиной " << strLength << " символов"<< std::endl;
+
+    return *this;
+}
--- MyString.h
+++ MyString.h
@@ -7,77 +7,53 @@
 class MyString {
     char* str;
     int strLength;
+public:
     static int strCount;
 
-public:
     /**
      * Контруктор принимающий длину строки
      * @param strLength Длина строки
      */
-    MyString (int strLength) {
-        str = new char[strLength] {NULL};
-        this->strLength = strLength;
-
-        std::cout << "Конструктор с установкой длины - " << this << std::endl;
-        std::cout << std::endl;
-
-        MyString::strCount++;
-    }
+    MyString (int strLength);
 
     /**
      * Контруктор по умолчанию, делегирует на контруктор принимющий длину строки
      */
-    MyString () : MyString (80){
-        std::cout << "Конструктор по умолчанию (делегировал) - " << this << std::endl;
-        std::cout << std::endl;
-    }
+    MyString ();
 
     /**
      * Конструктор принимающий строку
      * @param strInput инициализированная строка
      */
-    MyString (char* strInput) : str {strInput} {
-        strLength = strlen(str);
-        std::cout << "Конструктор принимающий строку - " << this << std::endl;
-        std::cout << std::endl;
+    MyString (char* strInput);
 
-        MyString::strCount++;
-    }
+    /**
+     * Конструктор копирования
+     *
+     * @param str объект для копирования
+     */
+    MyString (const MyString& strP);
 
     /**
      * Деструктор
      */
-    ~MyString () {
-        std::cout << "Уничтожен объект " << this << std::endl;
-        std::cout << std::endl;
-        str = nullptr;
-        delete str;
-    }
+    ~MyString ();
 
     /**
      * Запись строки с консоли в строку объекта
      * @return this
      */
-    MyString& addStr () {
-        std::cout << "Введите строку: " << std::endl;
-        std::cin.getline(str, strLength); // проверка на переполнение строки
-
-        return *this;
-    }
+    MyString& addStr ();
 
     /**
      * Вывод строки
      * @return this
      */
-    MyString& printStr () {
-        std::cout << str << std::endl;
-        std::cout << "Выведена строка длиной " << strLength << " символов"<< std::endl;
-        return *this;
-    }
+    MyString& printStr ();
 
     /**
-     * Вывод количества созданных экземпляров
-     */
+    * Вывод количества созданных экземпляров
+    */
     static void printStrCount () {
         std::cout << "Всего создано " << strCount << " строк"<< std::endl;
     }
